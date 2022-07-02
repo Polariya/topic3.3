1e271246b0b1b41b59c6f200fc97731189b52dcf
Polina Grigorieva <polariyagv@mail.ru>
2022-07-02 10:32:26 +0500

    task21

---
+++ task21/Program.cs
@@ -0,0 +1,13 @@
+﻿double x, y, z, x1, y1, z1, len;
+x = Convert.ToDouble(Console.ReadLine());
+y = Convert.ToDouble(Console.ReadLine());
+z = Convert.ToDouble(Console.ReadLine());
+x1 = Convert.ToDouble(Console.ReadLine());
+y1 = Convert.ToDouble(Console.ReadLine());
+z1 = Convert.ToDouble(Console.ReadLine());
+len = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1) + (z - z1) * (z - z1) );
+Console.WriteLine(len);
+
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
