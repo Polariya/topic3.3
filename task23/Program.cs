a60ff3b260ad68f3d1e65f33d66d7e540dba720e
Polina Grigorieva <polariyagv@mail.ru>
2022-07-02 10:32:50 +0500

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,9 @@
+﻿int n, i = 1;
+n = Convert.ToInt32(Console.ReadLine());
+while (i <= n){
+    Console.WriteLine(i * i * i);
+    i++;     
+}
+
+
+//Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N.
